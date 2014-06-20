da6ffae2c8564ebe17b273d4ae2657e41c292cdf
Surendra Shrestha <shopuz@gamil.com>
2014-06-20 12:26:10 +1000

    validating longitude, latitude and user_id added for bulk upload

--- app/models/import.rb
+++ app/models/import.rb
@@ -23,9 +23,21 @@ class Import
   def save
     puts "controller_name :"
     puts model_name
-    true
+    
+    imported_products.each do |product|
+      if product.errors.any?
+        imported_products.each_with_index do |product, index|
+          product.errors.full_messages.each do |message|
+            errors.add :base, "Row #{index+2}: #{message}"
+          end
+        end
+        return false
+      end
 
-    if imported_products.map(&:valid?).all?
+    end
+
+    
+    if imported_products.map(&:valid?).all? 
       imported_products.each(&:save!)
       true
     else
@@ -49,8 +61,23 @@ class Import
     (2..spreadsheet.last_row).map do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       product = model_name.find_by_id(row["id"]) || model_name.new
-      puts product.attributes
+      
+      
       product.attributes = row.to_hash
+      #puts "product attributes"
+      #puts product.attributes["user_id"]
+      validate_user(product, product.attributes["user_id"])
+      
+      if product.attributes["latitude"]
+        validate_latitude(product, product.attributes["latitude"], "latitude", -90, 90)
+      end
+
+      if product.attributes["longitude"]
+        validate_latitude(product, product.attributes["longitude"], "longitude",  -180, 180)
+      end
+
+      puts "product info: "
+      puts product
       product
     end
   end
@@ -58,9 +85,27 @@ class Import
   def open_spreadsheet
     case File.extname(file.original_filename)
     when ".csv" then Roo::CSV.new(file.path)
-    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Excel.new(file.path)
+    when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
+
+  def validate_user(product, user_id)
+    if User.where(id: user_id).nil?
+      product.errors[:base] << "Invalid user with id: " + user_id.to_s
+    end
+
+  end
+
+  def validate_latitude(product, val, item, start, finish)
+    puts "validating #{item}"
+    val = val.to_i
+    if val < start or val > finish
+      product.errors[:base] << "Invalid #{item}: "  + val.to_s + " ( has to be between #{start} and #{finish} ) "
+      puts "latitude error"
+    end
+  end
+
+
 end
