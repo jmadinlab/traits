c26c6a37d3a1664aff136c79cb1f91a6e132c0ef
Joshua Madin <joshua.madin@mq.edu.au>
2015-03-31 20:58:19 +1100

    Date fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 require 'base_ext'
 # Initialize the rails application.
 Traits::Application.initialize!
+
+Time::DATE_FORMATS[:ctdb_date] = "%e %B %Y at %I:%M %p"
