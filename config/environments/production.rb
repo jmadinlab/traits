1f036b959827ccf36deb633259a8def94a4bcc72
Joshua Madin <joshua.madin@mq.edu.au>
2015-09-23 09:58:40 +1000

    Temporary relax of user details required to sort out password reset issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Traits::Application.configure do
   
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    # port: 587,
+    port: 465,
     domain: ENV["SITE_URL"],
     authentication: "plain",
     enable_starttls_auto: true,
