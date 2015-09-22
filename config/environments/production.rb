17d6ba57e5b30741b112dd06f8694dcbbb00bd6b
Joshua Madin <joshua.madin@mq.edu.au>
2015-09-23 08:23:19 +1000

    Temporary relax of user details required to sort out password reset issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,8 @@ config.action_mailer.raise_delivery_errors = true
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     enable_starttls_auto: true,
-    ssl: true
+    ssl: true,
+    openssl_verify_mode: 'none'
   }  
 # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
