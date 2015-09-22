5c7a3e003c8d2b02e669f088d7c96020380015b1
Joshua Madin <joshua.madin@mq.edu.au>
2015-09-23 09:47:08 +1000

    Temporary relax of user details required to sort out password reset issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,23 @@ Traits::Application.configure do
   
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 2525,
+    port: 587,
     domain: ENV["SITE_URL"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
-    enable_starttls_auto: true,
     ssl: true
+
+    # address: "smtp.gmail.com",
+    # port: 2525,
+    # domain: ENV["SITE_URL"],
+    # authentication: "plain",
+    # enable_starttls_auto: true,
+    # user_name: ENV["GMAIL_USERNAME"],
+    # password: ENV["GMAIL_PASSWORD"],
+    # enable_starttls_auto: true,
+    # ssl: true
     # openssl_verify_mode: 'none'
   }  
 # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
