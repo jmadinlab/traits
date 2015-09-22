dbe4a9feeec911b68b5854f0198ce35a755196ef
Joshua Madin <joshua.madin@mq.edu.au>
2015-09-23 07:59:53 +1000

    Temporary relax of user details required to sort out password reset issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
-  validates :last_name, presence: true, length: { maximum: 50 }
-  validates :institution, presence: true, length: { maximum: 50 }
+  # validates :last_name, presence: true, length: { maximum: 50 }
+  # validates :institution, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
