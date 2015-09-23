7e5532fe12981486c97e48576c79141d9d527100
Joshua Madin <joshua.madin@mq.edu.au>
2015-09-23 10:06:05 +1000

    Temporary relax of user details required to sort out password reset issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,14 @@ Traits::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "coraltraits.org" }
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: "coraltraits.org" }
+  # config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 465,
+    port: 587,
     domain: ENV["SITE_URL"],
     authentication: "plain",
     enable_starttls_auto: true,
