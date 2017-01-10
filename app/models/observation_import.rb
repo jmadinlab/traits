6139a84228cba6a23540831a56d5672c63a69691
Joshua Madin <joshua.madin@mq.edu.au>
2017-01-10 14:53:58 +1100

    dealt with no resource and private observation uploads.

--- app/models/observation_import.rb
+++ app/models/observation_import.rb
@@ -367,7 +367,7 @@ class ObservationImport
       puts "specie_id: #{row["specie_id"].present?}".green
       $observation.errors[:base] << "Row #{i}: Specie_id=#{row["specie_id"]} doesn't exist" if Specie.where("id = ?", row["specie_id"]).blank?
       $observation.errors[:base] << "Row #{i}: Location_id=#{row["location_id"]} doesn't exist" if Location.where("id = ?", row["location_id"]).blank?
-      $observation.errors[:base] << "Row #{i}: Resource_id=#{row["resource_id"]} doesn't exist" if Resource.where("id = ?", row["resource_id"]).blank?
+      # $observation.errors[:base] << "Row #{i}: Resource_id=#{row["resource_id"]} doesn't exist" if Resource.where("id = ?", row["resource_id"]).blank?
       $observation.errors[:base] << "Row #{i}: Resource_id=#{row["resource_id"]} doesn't exist and access is public" if (Resource.where("id = ?", row["resource_id"]).blank? and not row["private"]) unless row["resource_id"].blank?
 
       $observation.errors[:base] << "Row #{i}: Secondary resource_id=#{row["resource_secondary_id"]} doesn't exist" if Resource.where("id = ?", row["resource_secondary_id"]).blank? unless row["resource_secondary_id"].blank?
