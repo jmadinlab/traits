6a8c3801d6771138a60ed5c55cc1243b2c8b6038
Joshua Madin <jmadin@hawaii.edu>
2018-06-05 09:34:45 -1000

    images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Traits::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
