00ebf33ddf77322a03fd2b2491ae5ff0081eb98b
Joshua Madin <jmadin@hawaii.edu>
2018-06-05 08:56:21 -1000

    images

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ append :linked_files, "config/database.yml", "config/local_env.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/assets/veron"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/assets/images"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
